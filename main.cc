dddb4c3052cab60fef638b66a1efcee00dd95992
Eugen <e.k.92@gmx.de>
2017-08-18 12:42:02 +0200

    Reihenfolge der Parameter geändert

--- main.cc
+++ main.cc
@@ -14,19 +14,20 @@ int main (int argc, char* argv[]) {
 	else {
 		std::string algo = "";
 		std::string start_word = "!";
-		std::string end_word = "~~~~~~~~~~";
+		// std::string end_word = "~~~~~~~~~~";
+		std::string end_word = "\"!!!";
 		std::string hashstring = argv[1];
 		if (argc > 2) {
-			algo = argv[2];
+			start_word = argv[2];
+		}
+		if (argc > 3) {
+			end_word = argv[3];
+		}
+		if (argc > 4) {
+			algo = argv[4];
 		}
 		else {
 			algo = find_algorithm(hashstring);
-		}
-		if (argc > 3) {
-			start_word = argv[3];
-		}
-		if (argc > 4) {
-			end_word = argv[4];
 		}
 
 		std::cout << std::endl << "Used algorithm: " << algo << std::endl;
